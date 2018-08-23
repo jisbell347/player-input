726e803eff10031f314bb4d5d1ff0dd862128bbe
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-08-23 16:34:01 -0600

    Added function to allow player to move left

--- Player.cs
+++ Player.cs
@@ -16,5 +16,9 @@ public class Player : MonoBehaviour {
 	    if (Input.GetKey("right")) {
 	        transform.position += Vector3.right * speed * Time.deltaTime;
 	    }
+
+	    if (Input.GetKey("left")) {
+	        transform.position += Vector3.left * speed * Time.deltaTime;
+	    }
 	}
 }
