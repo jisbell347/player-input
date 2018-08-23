636cd0a88a60afe88f40d63018fbf43e70604af3
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-08-23 16:30:59 -0600

    Added function to allow player to move right

---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 3.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey("right")) {
+	        transform.position += Vector3.right * speed * Time.deltaTime;
+	    }
+	}
+}
