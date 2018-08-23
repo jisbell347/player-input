c62bdf6f51f388236dd6247979bc001264253504
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-08-23 15:14:13 -0600

    Added functions to listen on key presses

--- GameController.cs
+++ GameController.cs
@@ -6,11 +6,21 @@ public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("Hello World!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown("space")) {
+            Debug.Log("Jump!");
+        }
+
+	    if (Input.GetKey("left")) {
+	        Debug.Log("Move Left!");
+	    }
+
+	    if (Input.GetKey("right")) {
+	        Debug.Log("Move Right!");
+	    }
 	}
 }
